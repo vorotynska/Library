d661e1498104e66406a7903630d985f0cc652b4c
Irina Carpincic <iryna.vorotynska@gmail.com>
2023-05-07 10:11:40 +0200

    commit

--- index.js
+++ index.js
@@ -21,3 +21,25 @@ function addBookToLibrary() {
     return book1.info()
 }
 addBookToLibrary()
+
+let user = {
+    name: "John",
+    surname: "Smith",
+
+    get fullName() {
+        return `${this.name} ${this.surname}`;
+    },
+
+    set fullName(value) {
+        [this.name, this.surname] = value.split(" ");
+    }
+};
+
+// set fullName is executed with the given value.
+user.fullName = "Alice Cooper";
+
+alert(user.name); // Alice
+alert(user.surname); // Cooper
+
+user.fullName = 'ira vort'
+alert(user.name)
